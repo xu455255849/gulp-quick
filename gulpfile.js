bde8857d12be801b92dad47cec942d2f976e79da
admin <455255849@qq.com>
2018-06-23 18:22:33 +0800

    up

--- gulpfile.js
+++ gulpfile.js
@@ -14,8 +14,6 @@ var gulp = require('gulp'),
     fileinclude = require('gulp-file-include'),
     browserSync = require('browser-sync').create(),
     cdnify = require('gulp-cdnify'),
-    concat = require('gulp-concat'),
-    rename = require("gulp-rename"),
     handleError = function (err) {
         var colors = gutil.colors;
         console.log('\n')
@@ -32,34 +30,12 @@ gulp.task('clean', function () {
 
 gulp.task('html', function () {
     return gulp.src('src/pages/*.html')
-    .pipe(cdnify({
-        base: 'http://static.kanhunli.cn/yunxi/official/v22/v22/',
-        html: {
-            'img[data-src]': 'data-src'
-        }
-    }))
-    .pipe(fileinclude())
-    .pipe(rename(function (path) {
-        path.extname = ".php";
-    }))
-    .pipe(gulp.dest('dist/pages'));
-});
-
-gulp.task('yun:html', function () {
-    gulp.watch('src/pages/*.html', function () {
-        gulp.src('src/pages/*.html')
-        .pipe(cdnify({
-            base: 'http://static.kanhunli.cn/yunxi/official/v22/v22/',
-            html: {
-                'img[data-src]': 'data-src'
-            }
-        }))
         .pipe(fileinclude())
-        .pipe(gulp.dest('site/pages'));
-    })
+        /*   .pipe(cdnify({
+               base: 'http://static.kanhunli.cn/yunxi/official/testfive/testfive/'
+           }))*/
+        .pipe(gulp.dest('dist/pages'));
 });
-
-
 gulp.task('server', function () {
     browserSync.init({
         server: {
@@ -80,19 +56,12 @@ gulp.task('server', function () {
 gulp.task('dev:copy', function () {
     gulp.watch('src/static/**/*', function () {
         gulp.src('src/static/**/*')
-        .pipe(gulp.dest('dist/static/'))
+            .pipe(gulp.dest('dist/static/'))
     })
 });
 gulp.task('build:copy', function () {
     gulp.src('src/static/**/*')
-    .pipe(gulp.dest('dist/static/'))
-});
-
-gulp.task('yun:copy', function () {
-    gulp.watch('src/static/**/*', function () {
-        gulp.src('src/static/**/*')
-        .pipe(gulp.dest('../site/static/'))
-    })
+        .pipe(gulp.dest('dist/static/'))
 });
 
 gulp.task('dev:image', function () {
@@ -101,54 +70,23 @@ gulp.task('dev:image', function () {
         gutil.log(gutil.colors.green(event.type) + ' ' + paths.srcPath);
         gutil.log('Dist ' + paths.distPath);
         gulp.src(paths.srcPath)
-        .pipe(imagemin({
-            progressive: true
-        }))
-        .pipe(gulp.dest(paths.distDir))
+            .pipe(imagemin({
+                progressive: true
+            }))
+            .pipe(gulp.dest(paths.distDir))
     })
 });
 gulp.task('build:image', function () {
     gulp.src('src/images/**/*')
-    .pipe(imagemin({
-        progressive: true
-    }))
-    .pipe(gulp.dest('dist/images'))
-});
-gulp.task('yun:image', function () {
-    gulp.watch('src/images/**/*', function (event) {
-        var paths = watchPath(event,'src/','../site/');
-        gutil.log(gutil.colors.green(event.type) + ' ' + paths.srcPath);
-        gutil.log('Dist ' + paths.distPath);
-        gulp.src(paths.srcPath)
         .pipe(imagemin({
             progressive: true
         }))
-        .pipe(gulp.dest(paths.distDir))
-    })
+        .pipe(gulp.dest('dist/images'))
 });
-
 
 gulp.task('dev:less', function () {
     gulp.watch('src/less/**/*.less', function (event) {
         var paths = watchPath(event, 'src/less/', 'dist/css/');
-        gutil.log(gutil.colors.green(event.type) + ' ' + paths.srcPath);
-        gutil.log('Dist ' + paths.distPath);
-        var combined = combiner.obj([
-            gulp.src(paths.srcPath),
-            sourcemaps.init(),
-            less({
-                plugins: [autoprefix]
-            }),
-            minifycss(),
-            sourcemaps.write('./'),
-            gulp.dest(paths.distDir)
-        ]);
-        combined.on('error', handleError)
-    })
-});
-gulp.task('yun:less', function () {
-    gulp.watch('src/less/**/*.less', function (event) {
-        var paths = watchPath(event, 'src/less/', '../site/css/');
         gutil.log(gutil.colors.green(event.type) + ' ' + paths.srcPath);
         gutil.log('Dist ' + paths.distPath);
         var combined = combiner.obj([
@@ -170,10 +108,10 @@ gulp.task('build:less', function () {
         less({
             plugins: [autoprefix]
         }),
-        //minifycss(),
-        cdnify({
-            base: 'http://static.kanhunli.cn/yunxi/official/v22/v22/'
-        }),
+        minifycss(),
+        /* cdnify({
+             base: 'http://static.kanhunli.cn/yunxi/official/testfive/testfive/'
+         }),*/
         gulp.dest('dist/css/')
     ]);
     combined.on('error', handleError)
@@ -182,30 +120,6 @@ gulp.task('build:less', function () {
 gulp.task('dev:js', function () {
     gulp.watch('src/js/**/*.js', function (event) {
         var paths = watchPath(event, 'src/', 'dist/');
-        gutil.log(gutil.colors.green(event.type) + ' ' + paths.srcPath);
-        gutil.log('Dist ' + paths.distPath);
-        var combined = combiner.obj([
-            gulp.src(paths.srcPath),
-            sourcemaps.init(),
-            babel({
-                presets: ['env'], plugins: [
-                    ["transform-runtime", {
-                        "helpers": false,
-                        "polyfill": false,
-                        "regenerator": true,
-                        "moduleName": "babel-runtime"
-                    }]
-                ] }),
-            uglify(),
-            sourcemaps.write('./'),
-            gulp.dest(paths.distDir)
-        ]);
-        combined.on('error', handleError)
-    })
-});
-gulp.task('yun:js', function () {
-    gulp.watch('src/js/**/*.js', function (event) {
-        var paths = watchPath(event, 'src/', '../site/');
         gutil.log(gutil.colors.green(event.type) + ' ' + paths.srcPath);
         gutil.log('Dist ' + paths.distPath);
         var combined = combiner.obj([
@@ -238,28 +152,11 @@ gulp.task('build:js', function () {
                     "moduleName": "babel-runtime"
                 }]
             ] }),
-        //uglify(),
+        uglify(),
         gulp.dest('dist/js/')
     ]);
     combined.on('error', handleError)
 });
-/*
 
-gulp.task('all:css', function () {
-    return gulp.src(['src/static/bootstrap.min.css', 'src/static/aos.css', 'select2.min.css', 'slick-theme'])
-    .pipe(concat('all.css'))
-    .pipe(gulp.dest('dist/css/'));
-});
-
-gulp.task('all:js', function () {
-    return gulp.src(['src/static/bootstrap.min.js', 'src/static/jquery.countup.min.js', 'jquery.waypoints.min.js', 'lazysizes.min.js', 'select2.min.js'])
-    .pipe(concat('all.js'))
-    .pipe(gulp.dest('dist/js/'));
-})
-*/
-
-
-
-gulp.task('build', ['clean'/*, 'build:copy'*/, 'build:js', 'build:less'/*, 'build:image'*/, 'html']);
+gulp.task('build', ['clean', 'build:copy', 'build:js', 'build:less', 'build:image', 'html']);
 gulp.task('default', ['clean', 'build:copy', 'dev:copy', 'build:js', 'build:less', 'build:image', 'dev:image', 'html', 'server']);
-gulp.task('dev', ['yun:html', 'yun:copy', 'yun:image', 'yun:less', 'yun:js']);
